d1b019ffce5c8e3c1599d5af03227612ba595a17
Riya Srivastava <riyasrivastava1701@gmail.com>
2025-08-05 20:10:48 +0530

    Server.js Modified

--- server.js
+++ server.js
@@ -29,7 +29,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`);
 });
-
-app.post("/register", async (req, res) => {
-  console.log("Register Server is working");
-});
